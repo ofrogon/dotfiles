041ed06a561a670568e5b36a0a4f4b8532c19fd1
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2025-08-28 20:44:32 -0400

    Add bat and btop to dotfiles

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -9,6 +9,11 @@ return {
     "catppuccin",
     opts = {
       transparent_background = true,
+      function()
+        local bufferline = require("catppuccin.groups.integrations.bufferline")
+        bufferline.get = bufferline.get or bufferline.get_theme
+      end,
     },
+    optional = true,dd
   },
 }
