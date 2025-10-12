fffec2a967ed63dba1988d0829931300354667c9
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2025-10-12 16:46:46 -0400

    Sound volume notifications, faster Walker and don't display sound percentage

--- .config/hypr/modes/clamshell.sh
+++ .config/hypr/modes/clamshell.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 notify_user() {
-  current=$(get_current_brightness)
-  notify-send -e -h string:x-canonical-private-synchronous:osd -u low "Écran du laptop $1"
+  notify-send -e -h string:x-canonical-private-synchronous:osd -u low "The laptop screen is $1"
 }
 
 if [[ "$1" == "--open" ]]; then
   hyprctl keyword monitor "eDP-1, preferred, 3440x700, 1"
-  notify_user "ouverte"
+  notify_user "open"
   hyprctl reload
 else
   hyprctl keyword monitor "eDP-1, disable"
-  notify_user "fermée"
+  notify_user "close"
 fi
--- .config/hypr/modes/sound.sh
+++ .config/hypr/modes/sound.sh
@@ -1,26 +1,18 @@
-#increse
-wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-#decrease
-wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-#mute
-wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-
-
-
 #!/bin/bash
 
-notofy_user() {
-  
+notify_user() {
+  current=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2 * 100) "%"}')
+  notify-send -e -h string:x-canonical-private-synchronous:osd -u low "$1 $current"
 }
 
 if [[ "$1" == "--increase" ]]; then
-  hyprctl keyword monitor "eDP-1, preferred, 3440x700, 1"
-  notify-send "Écran du laptop ouverte"
-  hyprctl reload
-elif [[ "$1" == "--decrease"]]
-
-elif [[ "$1" == "--toggle-mute" ]]
-
+  wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+  notify_user "Increased sound to "
+elif [[ "$1" == "--decrease" ]]; then
+  wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+  notify_user "Decreased sound to "
+elif [[ "$1" == "--toggle-mute" ]]; then
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
 else
   notify-send "Unknown parameter"
 fi
